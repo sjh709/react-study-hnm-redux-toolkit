9152b636c60f27b914ccb8518ba078a585b8aa4e
Jihee <sjh709@naver.com>
2024-03-24 20:24:54 +0900

    json server

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
-  return <div>전체 상품 페이지</div>;
+  const [productList, setProductList] = useState([]);
+
+  const getProducts = async () => {
+    let url = `http://localhost:4000/products`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setProductList(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <div>
+      <ProductCard />
+    </div>
+  );
 };
 
 export default ProductAll;
