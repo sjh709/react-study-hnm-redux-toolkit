f0d243639715cf62567751f064344ae624fc52b2
Jihee <sjh709@naver.com>
2024-03-30 03:21:39 +0900

    combineReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
-import productReducer from './reducers/productReducer';
 import { thunk } from 'redux-thunk';
+import rootReducer from './reducers';
 
-let store = createStore(productReducer, applyMiddleware(thunk));
+let store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
