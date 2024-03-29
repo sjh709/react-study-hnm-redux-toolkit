116f0315ce53fb575dd9b5003ce0b21fcd0973cb
Jihee <sjh709@naver.com>
2024-03-30 04:13:37 +0900

    json server url 수정

--- src/redux/actions/productAction.js
+++ src/redux/actions/productAction.js
@@ -1,6 +1,6 @@
 function getProducts(searchQuery) {
   return async (dispatch, getState) => {
-    let url = `http://localhost:4000/products?q=${searchQuery}`;
+    let url = `https://my-json-server.typicode.com/sjh709/react-study-hnm-redux/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
     dispatch({ type: 'GET_PRODUCT_SUCCESS', payload: { data } });
@@ -9,7 +9,7 @@ function getProducts(searchQuery) {
 
 function getProductDetail(id) {
   return async (dispatch) => {
-    let url = `http://localhost:4000/products/${id}`;
+    let url = `https://my-json-server.typicode.com/sjh709/react-study-hnm-redux/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     dispatch({ type: 'GET_SINGLE_PRODUCT_SUCCESS', payload: { data } });
